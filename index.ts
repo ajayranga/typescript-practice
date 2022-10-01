d4946b421ab57c04dd3e95dbecac48f4a9d09f6e
Ajay <ajay.ranga.mr.unique@gmail.com>
2022-10-01 11:58:21 +0530

    tuple and array

--- index.ts
+++ index.ts
@@ -1,7 +1,14 @@
-const person = {
+type person = {
+  name: string;
+  age: number;
+  hobbies: string[]; // array of string can be of any length
+  tuple: [number, string];
+};
+const person: person = {
   name: 'Maximilian',
   age: 30,
   hobbies: ['Sports', 'Cooking'],
+  tuple: [13, 'hello'],
 };
 
 let favoriteActivities: string[];
@@ -11,4 +18,7 @@ console.log(person.name);
 
 for (const hobby of person.hobbies) {
   console.log(hobby.toUpperCase());
+}
+for (const tpl of person.tuple) {
+  console.log(tpl);
 }
