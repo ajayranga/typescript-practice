3125672eafc7cfe47f8008411547d2bd112d69c7
Ajay <ajay.ranga.mr.unique@gmail.com>
2022-10-01 12:11:33 +0530

    enums implemented

--- index.ts
+++ index.ts
@@ -3,18 +3,26 @@ type person = {
   age: number;
   hobbies: string[]; // array of string can be of any length
   tuple: [number, string];
+  role: Role;
 };
+enum Role {
+  ADMIN = 'ADMIN',
+  READ_ONLY = 100,
+  AUTHOR = 'AUTHOR',
+}
 const person: person = {
   name: 'Maximilian',
   age: 30,
   hobbies: ['Sports', 'Cooking'],
   tuple: [13, 'hello'],
+  role: Role.ADMIN,
 };
 
 let favoriteActivities: string[];
 favoriteActivities = ['Sports'];
 
 console.log(person.name);
+console.log(person.role);
 
 for (const hobby of person.hobbies) {
   console.log(hobby.toUpperCase());
