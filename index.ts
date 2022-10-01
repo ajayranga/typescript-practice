faf5bb0f6bc4f919c6c716ed83056c8e4f27e2f2
Ajay <ajay.ranga.mr.unique@gmail.com>
2022-10-01 12:24:18 +0530

    type alias type literal unions functions never in typescript

--- index.ts
+++ index.ts
@@ -1,9 +1,15 @@
+type someAlias = number | string;
 type person = {
   name: string;
   age: number;
   hobbies: string[]; // array of string can be of any length
   tuple: [number, string];
   role: Role;
+  uniondt: string | number;
+  literalType: 'some literal' | 'some other literal or value' | 5;
+  agf?: someAlias;
+  sqr(): number;
+  sqrprnt(): never;
 };
 enum Role {
   ADMIN = 'ADMIN',
@@ -16,6 +22,15 @@ const person: person = {
   hobbies: ['Sports', 'Cooking'],
   tuple: [13, 'hello'],
   role: Role.ADMIN,
+  uniondt: 1,
+  literalType: 'some literal',
+  sqr: function sqr() {
+    return this.age * this.age;
+  },
+  sqrprnt: function sqrprnt() {
+    console.log(this.age * this.age);
+    throw Error('some error');
+  },
 };
 
 let favoriteActivities: string[];
@@ -23,6 +38,8 @@ favoriteActivities = ['Sports'];
 
 console.log(person.name);
 console.log(person.role);
+console.log(person.uniondt);
+console.log(person.sqr());
 
 for (const hobby of person.hobbies) {
   console.log(hobby.toUpperCase());
