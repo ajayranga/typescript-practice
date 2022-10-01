df7bc9c994e48ccf90d0341dd2d79f7d8236fdb4
Ajay <ajay.ranga.mr.unique@gmail.com>
2022-10-01 16:26:43 +0530

    done

--- index.ts
+++ index.ts
@@ -1,49 +1,24 @@
-type someAlias = number | string;
-type person = {
-  name: string;
-  age: number;
-  hobbies: string[]; // array of string can be of any length
-  tuple: [number, string];
-  role: Role;
-  uniondt: string | number;
-  literalType: 'some literal' | 'some other literal or value' | 5;
-  agf?: someAlias;
-  sqr(): number;
-  sqrprnt(): never;
+console.log('hello');
+var person = {
+  name: 'aaa',
+  age: 50,
 };
-enum Role {
-  ADMIN = 'ADMIN',
-  READ_ONLY = 100,
-  AUTHOR = 'AUTHOR',
-}
-const person: person = {
-  name: 'Maximilian',
-  age: 30,
-  hobbies: ['Sports', 'Cooking'],
-  tuple: [13, 'hello'],
-  role: Role.ADMIN,
-  uniondt: 1,
-  literalType: 'some literal',
-  sqr: function sqr() {
-    return this.age * this.age;
-  },
-  sqrprnt: function sqrprnt() {
-    console.log(this.age * this.age);
-    throw Error('some error');
-  },
-};
-
-let favoriteActivities: string[];
-favoriteActivities = ['Sports'];
 
 console.log(person.name);
-console.log(person.role);
-console.log(person.uniondt);
-console.log(person.sqr());
 
-for (const hobby of person.hobbies) {
-  console.log(hobby.toUpperCase());
-}
-for (const tpl of person.tuple) {
-  console.log(tpl);
-}
+((num) => console.log(num * num))(5);
+
+const newPerson = person;
+const newPerson2 = { ...person };
+console.log(newPerson, newPerson2);
+const add = (...numb: number[]) =>
+  numb.reduce((prev, current) => prev + current, 55);
+
+console.log(add(1, 2, 8, 5));
+
+const arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+const [a, b, c, ...rem] = arr1;
+const { name: fName, age } = person;
+
+console.log(a, b, c, rem, fName);
+// export {};
