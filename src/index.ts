682e38b911f7935d53005f3d106f8a1b01359a9c
Ajay <ajay.ranga.mr.unique@gmail.com>
2022-10-01 16:08:28 +0530

    merge

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log('first');
+type person = {
+  name: string;
+  age: number;
+  hobbies: string[]; // array of string can be of any length
+  tuple: [number, string];
+};
+const person: person = {
+  name: 'Maximilian',
+  age: 30,
+  hobbies: ['Sports', 'Cooking'],
+  tuple: [13, 'hello'],
+};
+
+let favoriteActivities: string[];
+favoriteActivities = ['Sports'];
+
+console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+}
+for (const tpl of person.tuple) {
+  console.log(tpl);
+}
